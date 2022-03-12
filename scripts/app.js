739efb71a7d1cdc0bd5a2d9377417a873e092f2b
Dev-Robin <58303612+Dev-Robin@users.noreply.github.com>
2022-03-12 17:29:00 +0100

    Added keyboard functionality and fixed minor bugs

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ let word;
 let guess;
 let guessArr = [];
 let grid = 1;
+let colum = 0;
 
 keyPress();
 function randWord() {
@@ -32,12 +33,10 @@ function keyPress() {
             addLetter(letter);
         // Check if the pressed key is enter
         } else if (e.keyCode == 13) {
-            checkWord();
-
-            guessArr.length = 0;
+            enterKey();
         // Check if the pressed key is backspace
         } else if (e.keyCode == 8) {
-            guessArr.length = guessArr.length - 1;
+            backSpaceKey();
         }   
     }
 }
@@ -45,10 +44,12 @@ function keyPress() {
 // Function to compare the guess to the word
 function checkWord() {
     // Set the seperate guessed letters in the array to a complete word to compare
-    guess = guessArr.toString();
+    guess = guessArr.join('');
+    console.log(guess);
     // Check if the word and the guess have a direct match
     if (guess == word) {
         console.log("You guessed the right word!");
+        alert("Het woord was: " + word);
     } else {
         // Check if the guess matches any letters with the word
         for (let i = 0; i <= guessArr.length; i++) {
@@ -58,9 +59,11 @@ function checkWord() {
                 if (guessArr[i] == wordArr[i]) {
                     // The guessed letter is at the right place
                     console.log(guessArr[i] + " is positioned at the right place!");
+                    document.getElementById(colum*5 + i + 1).style.backgroundColor = "#6db347";
                 } else {
                     // The guessed letter is not at the right place but the word does contain the guessed letter
                     console.log("The word contains: " + guessArr[i]);
+                    document.getElementById(colum*5 + i + 1).style.backgroundColor = "#dbd24d";
                 }
             } else {
                 // The guessed letter is not in the word
@@ -73,14 +76,43 @@ function checkWord() {
 // Add a typed letter to the grid
 function addLetter(letter) {
     if (guessArr.length < 5) {
-        // Convert grid to a string so it matches with the id of grid-item div
-        grid.toString();
         // Set grid-item to the typed letter
         document.getElementById(grid).innerHTML = letter;
         guessArr.push(letter);
         console.log(guessArr);
-        parseInt(grid);
         grid++;
+    }
+}
+
+// Get value of the pressed key
+function keyBoard(button) {
+    letter = button.getAttribute("data-key");
+    console.log(letter);
+    addLetter(letter);
+}
+
+// Add functionality to backspace key
+function backSpaceKey() {
+    guessArr.length = guessArr.length - 1;
+    
+    // Update GUI
+    grid = grid - 1;
+    document.getElementById(grid).innerHTML = '';
+}
+
+// Add functionality to enter key
+function enterKey() {
+    if (guessArr.length == 5) {
+        checkWord();
+
+        colum = colum + 1;
+        guessArr.length = 0;
+        if (guess == word) {
+            reset();
+        }
+    }   
+    if (colum >= 6) {
+        reset();
     }
 }
 
@@ -88,10 +120,11 @@ function addLetter(letter) {
 function reset() {
     // For every tile on the board; clear the text
     for (let i = 1; i <= 30; i++) {
-        i.toString();
         document.getElementById(i).innerHTML = "";
+        document.getElementById(i).style.backgroundColor = "#272727";
         guessArr.length = 0;
-        parseInt(i);
+        colum = 0;
+        grid = 1;
     }
     // Pick a new secret word
     randWord();
