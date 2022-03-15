b4c931bec84023f57280c23f3c0688664c912806
Dev-Robin <58303612+Dev-Robin@users.noreply.github.com>
2022-03-15 13:49:40 +0100

    Fixed a few bugs

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ let colum = 0;
 keyPress();
 function randWord() {
     // Get a random number between 0 and 908
-    const rand = Math.floor(Math.random() * 908);
+    const rand = Math.floor(Math.random() * words.length);
 
     word = words[rand];
     console.log(word);
@@ -43,13 +43,16 @@ function keyPress() {
 
 // Function to compare the guess to the word
 function checkWord() {
-    // Set the seperate guessed letters in the array to a complete word to compare
-    guess = guessArr.join('');
+    
     console.log(guess);
     // Check if the word and the guess have a direct match
     if (guess == word) {
         console.log("You guessed the right word!");
-        alert("Het woord was: " + word);
+        for (let i = 0; i <= guessArr.length - 1; i++) {
+            document.getElementById(colum*5 + i + 1).style.backgroundColor = "#6db347";
+            document.getElementById(colum*5 + i + 1).style.borderColor = "#6db347";
+        }
+        alert("Je hebt het goed geraden! Het woord was: " + word);
     } else {
         // Check if the guess matches any letters with the word
         for (let i = 0; i <= guessArr.length; i++) {
@@ -61,15 +64,27 @@ function checkWord() {
                     console.log(guessArr[i] + " is positioned at the right place!");
                     // Paint the square of the correct letter green
                     document.getElementById(colum*5 + i + 1).style.backgroundColor = "#6db347";
+                    document.getElementById(colum*5 + i + 1).style.borderColor = "#6db347";
+                    document.getElementById(guessArr[i]).style.backgroundColor = "#6db347";
                 } else {
                     // The guessed letter is not at the right place but the word does contain the guessed letter
                     console.log("The word contains: " + guessArr[i]);
                     // Paint the square of the correct letter yellow
                     document.getElementById(colum*5 + i + 1).style.backgroundColor = "#dbd24d";
+                    document.getElementById(colum*5 + i + 1).style.borderColor = "#dbd24d";
+                    document.getElementById(guessArr[i]).style.backgroundColor = "#dbd24d";
                 }
             } else {
                 // The guessed letter is not in the word
                 console.log("The word does not contain: " + guessArr[i]);
+                // Paint the square of the incorrect letter grey
+                if (colum*5 + i + 1 != colum*5 + 6) {
+                    document.getElementById(colum*5 + i + 1).style.backgroundColor = "#454545";
+                    document.getElementById(colum*5 + i + 1).style.borderColor = "#454545";
+                }
+                if (guessArr[i] != null) {
+                    document.getElementById(guessArr[i]).style.backgroundColor = "#454545";
+                }
             }
         }
     }
@@ -104,18 +119,29 @@ function backSpaceKey() {
 
 // Add functionality to enter key
 function enterKey() {
-    if (guessArr.length == 5) {
-        checkWord();
-
-        colum = colum + 1;
-        guessArr.length = 0;
-        if (guess == word) {
-            reset();
+    // Set the seperate guessed letters in the array to a complete word to compare
+    guess = guessArr.join('');
+    // Check if the guess is a word from the word list
+    for (let i = 0; i <= words.length + 1; i++) {
+        if (guess == words[i]) {
+            checkWord();
+        
+            colum = colum + 1;
+            guessArr.length = 0;
+            if (guess == word) {
+                reset();
+            }
+            if (colum >= 6) {
+                // Reset is max amount of guesses is reached
+                alert("Je hebt al je beurten gespeeld. Het woord was: " + word);
+                reset();
+            }
+            break;
+        } else if (i == words.length + 1) {
+            alert("Dit woord staat niet in de lijst. Probeer een ander woord.");
+            break;
         }
     }   
-    if (colum >= 6) {
-        reset();
-    }
 }
 
 // Reset the board for the next game
@@ -124,10 +150,19 @@ function reset() {
     for (let i = 1; i <= 30; i++) {
         document.getElementById(i).innerHTML = "";
         document.getElementById(i).style.backgroundColor = "#272727";
+        document.getElementById(i).style.borderColor = "#979494";
         guessArr.length = 0;
         colum = 0;
-        grid = 1;
+        grid = 1;       
     }
+    // Reset keyboard colors
+    let kbButtons = document.getElementsByTagName('button');
+
+    for(let i = 0; i < kbButtons.length; i++) {
+        kbButtons[i].style.backgroundColor = "#b6b4b4";
+    }
+
+    
     // Pick a new secret word
     randWord();
 }
